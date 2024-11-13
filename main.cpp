92afd107b7532da9cd3863187bba2a752e650f10
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-11-13 22:16:35 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int b, bA, h, somma, rimanente, hp;
+
+    //Inizializzione variabili
+    bA=0;
+    somma=0;
+    h=0;
+
+    cout<<"Inserire il numero di blocchi"<<endl;
+    cin>>b;
+
+    while (somma<b)
+    {
+        bA=bA+1;
+
+        somma=somma+bA;
+
+        h=bA
+    }
+
+    //Verifica se i blocchi bastano
+
+    if (somma>b)
+    {
+        cout<<"Non ci sono abbastanza blocchi"<<endl;
+
+        h=h-1;
+    }
+
+    cout<<"L'altezza è "<<h<<endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
